868fe149690c6daf45e164345ea9326159514162
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-05 09:43:03 +0530

    Update app.py

--- phonebook/image_for_web_server/app.py
+++ phonebook/image_for_web_server/app.py
@@ -26,7 +26,7 @@ def init_phonebook_db():
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL
     number VARCHAR(100) NOT NULL,
-    PRIMARY KEY
+    PRIMARY 
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_
    
     cursor.execute(phonebook_
